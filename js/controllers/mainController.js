37ce0a4b89ebc0c27849563f8bd56a9474e4b67e
Pegas <pegas344@gmail.com>
2017-01-28 21:41:05 +0200

    changes1

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,6 +1,11 @@
 app.controller('mainController', function($scope) {
     $scope.productsInCard = [];
     $scope.cartCount      = 0;
+    $scope.cartTotal      = 0;
+    $scope.min            = 1;
+    $scope.max            = 25;
+    $scope.disabled       = 'disabled';
+
     $scope.products       = [
         {
             image       : 'img/iphone1.jpg',
@@ -37,23 +42,27 @@ app.controller('mainController', function($scope) {
     ];
 
     $scope.addCard = function(index) {
-        $scope.products[index].classButton = 'disabled';
-        $scope.products[index].disabled    = true;
         $scope.products[index].cardTxt     = 'In card';
+        $scope.products[index].classButton = $scope.disabled;
+        $scope.products[index].disabled    = true;
         $scope.products[index].left       -= 1;
-        $scope.cartCount                  += 1;
 
         $scope.addProductInCard(index);
+        $scope.cartCount = $scope.productsInCard.length;
+        $scope.cartTotal = cartTotalSum($scope.productsInCard);
     };
 
     $scope.addProductInCard = function(index) {
-        var item      = {};
+        var item          = {};
 
-        item.index    = index;
-        item.image    = $scope.products[index].image;
-        item.name     = $scope.products[index].name;
-        item.price    = $scope.products[index].price;
-        item.shipping = $scope.products[index].shipping;
+        item.index        = index;
+        item.image        = $scope.products[index].image;
+        item.name         = $scope.products[index].name;
+        item.price        = $scope.products[index].price;
+        item.shipping     = $scope.products[index].shipping;
+        item.countProduct = 1;
+        item.classMinus   = $scope.disabled;
+        item.classPlus    = '';
 
         $scope.productsInCard.push(item);
     };
@@ -65,8 +74,36 @@ app.controller('mainController', function($scope) {
         $scope.products[productIndex].classButton = '';
         $scope.products[productIndex].disabled    = false;
         $scope.products[productIndex].left       += 1;
-        $scope.cartCount                         -= 1;
+
         $scope.productsInCard.splice(index, 1);
+        $scope.cartCount = $scope.productsInCard.length;
+        $scope.cartTotal = cartTotalSum($scope.productsInCard);
+    };
+
+    $scope.countMinus = function(index) {
+        var count = $scope.productsInCard[index].countProduct -= 1;
+
+        if (count <= $scope.min) {
+            $scope.productsInCard[index].countProduct = $scope.min;
+            $scope.productsInCard[index].classMinus   = $scope.disabled;
+        } else {
+            $scope.productsInCard[index].countProduct = count;
+            $scope.productsInCard[index].classPlus    = '';
+        }
+        $scope.cartTotal = cartTotalSum($scope.productsInCard);
+    };
+
+    $scope.countPlus = function(index) {
+        var count = $scope.productsInCard[index].countProduct += 1;
+
+        if (count >= $scope.max) {
+            $scope.productsInCard[index].countProduct = $scope.max;
+            $scope.productsInCard[index].classPlus    = $scope.disabled;
+        } else {
+            $scope.productsInCard[index].countProduct = count;
+            $scope.productsInCard[index].classMinus   = '';
+        }
+        $scope.cartTotal = cartTotalSum($scope.productsInCard);
     };
 
     $scope.search = function(product) {
@@ -87,5 +124,15 @@ app.controller('mainController', function($scope) {
         }
 
         return itemString;
+    };
+
+    var cartTotalSum = function(array) {
+        var sum = 0;
+
+        for (var i = 0; i < array.length; i++) {
+            sum += array[i].price * array[i].countProduct + array[i].shipping;
+        }
+
+        return sum;
     };
 });
