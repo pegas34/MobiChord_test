be18e1edfd7fbc13082738aa29b11765887f8060
Pegas <pegas344@gmail.com>
2017-01-30 00:34:03 +0200

    changes1

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,12 +1,19 @@
-app.controller('mainController', function($scope) {
-    $scope.productsInCard = [];
+app.controller('mainController', ['$scope', '$cookies', function($scope, $cookies) {
     $scope.cartCount      = 0;
     $scope.cartTotal      = 0;
     $scope.min            = 1;
     $scope.max            = 25;
-    $scope.disabled       = 'disabled';
-    $scope.cartShowClass  = 'hide';
     $scope.cartHeight     = {};
+    $scope.classes        = {
+        hide          : 'hide',
+        show          : 'show',
+        disabled      : 'disabled'
+    };
+    $scope.texts          = {
+        textButtonOn  : 'Add to cart',
+        textButtonOff : 'In cart'
+    };
+    $scope.cartShowClass  = $scope.classes.hide;
 
     $scope.products       = [
         {
@@ -21,7 +28,7 @@ app.controller('mainController', function($scope) {
             price       : 680.99,
             shipping    : 12.99,
             left        : 9,
-            cardTxt     : 'Add to cart',
+            cartTxt     : $scope.texts.textButtonOn,
             classButton : '',
             disabled    : false
         },
@@ -30,7 +37,7 @@ app.controller('mainController', function($scope) {
             name        : 'Moto Z Play with Style Mod (Black, 32GB)',
             infoList: [
                 {info : '13.97 centimeters (5.5-inch) super AMOLED 1080p full HD Display'},
-                {info : '2 GHz Octa-Core Qualcomm Snapdragon processor'},
+                {info : '2GHz Octa-Core Qualcomm Snapdragon processor'},
                 {info : '3GB RAM, 32GB internal memory expandable up to 2TB'},
                 {info : '3510 mAh all day battery with TurboPower charging'},
                 {info : 'Android 6.0.1 Marshmallow'},
@@ -38,8 +45,8 @@ app.controller('mainController', function($scope) {
             ],
             price       : 428.5,
             shipping    : 12.98,
-            left        : 11,
-            cardTxt     : 'Add to cart',
+            left        : 5,
+            cartTxt     : $scope.texts.textButtonOn,
             classButton : '',
             disabled    : false
         },
@@ -48,16 +55,16 @@ app.controller('mainController', function($scope) {
             name        : 'Xiaomi Mi Max Prime (Gold, 128GB)',
             infoList: [
                 {info : '16.35 centimeters (6.44-inch) IPS 2.5D glass capacitive touchscreen with 1920 x 1080 pixels resolution'},
-                {info : '64-bit Qualcomm Snapdragon core processor'},
+                {info : '2GHz 64-bit Qualcomm Snapdragon core processor'},
                 {info : '4GB RAM, 128GB internal memory'},
                 {info : '4850 mAH lithium-polymer battery'},
-                {info : 'Android 6.0.1 Marshmallow OS with MIUI 8'},
+                {info : 'Android 6.0.1 Marshmallow'},
                 {info : '16MP primary camera and 5MP front facing camera'}
             ],
             price       : 314.25,
             shipping    : 12.98,
             left        : 15,
-            cardTxt     : 'Add to cart',
+            cartTxt     : $scope.texts.textButtonOn,
             classButton : '',
             disabled    : false
         },
@@ -73,84 +80,127 @@ app.controller('mainController', function($scope) {
             price       : 685.99,
             shipping    : 12.98,
             left        : 3,
-            cardTxt     : 'Add to cart',
+            cartTxt     : $scope.texts.textButtonOn,
+            classButton : '',
+            disabled    : false
+        },
+        {
+            image       : 'img/lenovo1.jpg',
+            name        : 'Lenovo Z2 Plus (Black, 64GB)',
+            infoList: [
+                {info : '12.7 centimeters (5-inch) LTPS LCD capacitive touchscreen with 1920 x 1080 pixels resolution'},
+                {info : '2.15Ghz Qualcomm Snapdragon 820, 4 custom Kryo cores'},
+                {info : '4GB DDR4 RAM, 64GB SmartSLC internal memory'},
+                {info : '3500mAh high density Li-Ion Battery'},
+                {info : 'Android 6.0.1 Marshmallow'},
+                {info : '13MP large pixel primary camera with Hybrid autofocus'}
+            ],
+            price       : 299.99,
+            shipping    : 12.98,
+            left        : 12,
+            cartTxt     : $scope.texts.textButtonOn,
             classButton : '',
             disabled    : false
         }
     ];
 
-    $scope.addCard = function(index) {
-        $scope.products[index].cardTxt     = 'In card';
-        $scope.products[index].classButton = $scope.disabled;
+    $scope.addToCart = function(index) {
+        $scope.products[index].cartTxt     = $scope.texts.textButtonOff;
+        $scope.products[index].classButton = $scope.classes.disabled;
         $scope.products[index].disabled    = true;
         $scope.products[index].left       -= 1;
 
-        $scope.addProductInCard(index);
-        $scope.cartCount = $scope.productsInCard.length;
-        $scope.cartTotal = cartTotalSum($scope.productsInCard);
+        $scope.productsInCart.push($scope.addProductInCart(index));
+        $scope.cartCount = $scope.productsInCart.length;
+        $scope.cartTotal = $scope.cartTotalSum($scope.productsInCart);
+        $scope.setCartHeight();
+        $scope.setCartCookie(index);
     };
 
-    $scope.addProductInCard = function(index) {
+    $scope.addProductInCart = function(index, count) {
         var item          = {};
+        count             = count || 1;
 
         item.index        = index;
         item.image        = $scope.products[index].image;
         item.name         = $scope.products[index].name;
         item.price        = $scope.products[index].price;
         item.shipping     = $scope.products[index].shipping;
-        item.countProduct = 1;
-        item.classMinus   = $scope.disabled;
-        item.classPlus    = '';
+        item.countProduct = count;
+        if (count <= 1) {
+            item.classMinus   = $scope.classes.disabled;
+        } else {
+            item.classMinus   = '';
+        }
+        if (count >= $scope.products[index].left) {
+            item.classPlus    = $scope.classes.disabled;
+        } else {
+            item.classPlus   = '';
+        }
 
-        $scope.productsInCard.push(item);
-        setCardHeight();
+        return item;
     };
 
-    $scope.delCard = function(index) {
-        var productIndex = $scope.productsInCard[index].index;
+    $scope.delCart = function(index) {
+        var productIndex = $scope.productsInCart[index].index;
 
-        $scope.products[productIndex].cardTxt     = 'Add to cart';
+        $scope.products[productIndex].cartTxt     = $scope.texts.textButtonOn;
         $scope.products[productIndex].classButton = '';
         $scope.products[productIndex].disabled    = false;
-        $scope.products[productIndex].left       += 1;
+        $scope.products[productIndex].left       += $scope.productsInCart[index].countProduct;
 
-        $scope.productsInCard.splice(index, 1);
-        $scope.cartCount = $scope.productsInCard.length;
-        $scope.cartTotal = cartTotalSum($scope.productsInCard);
+        $scope.productsInCart.splice(index, 1);
+        $scope.cartCount = $scope.productsInCart.length;
+        $scope.cartTotal = $scope.cartTotalSum($scope.productsInCart);
         if ($scope.cartCount == 0) {
-            $scope.cartShowClass = 'hide';
+            $scope.cartShowClass = $scope.classes.hide;
         }
-        setCardHeight();
+        $scope.setCartHeight();
+        $scope.removeCartCookie(productIndex);
     };
 
     $scope.countMinus = function(index) {
-        var count = $scope.productsInCard[index].countProduct -= 1;
+        var count = $scope.productsInCart[index].countProduct - 1,
+            left  = $scope.products[$scope.productsInCart[index].index].left + 1;
 
-        if (count <= $scope.min) {
-            $scope.productsInCard[index].countProduct = $scope.min;
-            $scope.productsInCard[index].classMinus   = $scope.disabled;
+        if (count < 1) {
+            $scope.productsInCart[index].classMinus   = $scope.classes.disabled;
+            count = $scope.productsInCart[index].countProduct;
         } else {
-            $scope.productsInCard[index].countProduct = count;
-            $scope.productsInCard[index].classPlus    = '';
+            $scope.products[$scope.productsInCart[index].index].left = left;
+            $scope.productsInCart[index].countProduct = count;
+            $scope.productsInCart[index].classPlus    = '';
+            if (count <= 1) {
+                $scope.productsInCart[index].classMinus    = $scope.classes.disabled;
+            }
         }
-        $scope.cartTotal = cartTotalSum($scope.productsInCard);
+
+        $scope.cartTotal = $scope.cartTotalSum($scope.productsInCart);
+        $scope.setCartCookie($scope.productsInCart[index].index, count);
     };
 
     $scope.countPlus = function(index) {
-        var count = $scope.productsInCard[index].countProduct += 1;
+        var count = $scope.productsInCart[index].countProduct + 1,
+            left  = $scope.products[$scope.productsInCart[index].index].left - 1;
 
-        if (count >= $scope.max) {
-            $scope.productsInCard[index].countProduct = $scope.max;
-            $scope.productsInCard[index].classPlus    = $scope.disabled;
+        if (left < 0) {
+            $scope.productsInCart[index].classPlus    = $scope.classes.disabled;
+            count = $scope.productsInCart[index].countProduct;
         } else {
-            $scope.productsInCard[index].countProduct = count;
-            $scope.productsInCard[index].classMinus   = '';
+            $scope.products[$scope.productsInCart[index].index].left = left;
+            $scope.productsInCart[index].countProduct = count;
+            $scope.productsInCart[index].classMinus   = '';
+            if (left <= 0) {
+                $scope.productsInCart[index].classPlus    = $scope.classes.disabled;
+            }
         }
-        $scope.cartTotal = cartTotalSum($scope.productsInCard);
+
+        $scope.cartTotal = $scope.cartTotalSum($scope.productsInCart);
+        $scope.setCartCookie($scope.productsInCart[index].index, count);
     };
 
     $scope.search = function(product) {
-        var productInfoList = arrayObjectToString(product.infoList);
+        var productInfoList = $scope.arrayObjectToString(product.infoList);
 
         if (!$scope.searchInput || product.name.indexOf($scope.searchInput) != -1 || productInfoList.indexOf($scope.searchInput) != -1) {
             return true;
@@ -159,26 +209,27 @@ app.controller('mainController', function($scope) {
         return false;
     };
 
-    $scope.openCard = function() {
-        if ($scope.cartShowClass == 'hide' && $scope.productsInCard.length > 0) {
-            $scope.cartShowClass = 'show';
-            setCardHeight();
+    $scope.openCart = function() {
+        if ($scope.cartShowClass == $scope.classes.hide && $scope.productsInCart.length > 0) {
+            $scope.cartShowClass = $scope.classes.show;
+            $scope.setCartHeight();
         } else {
-            $scope.cartShowClass = 'hide';
+            $scope.cartShowClass = $scope.classes.hide;
         }
+        $scope.cartTotal = $scope.cartTotalSum($scope.productsInCart);
     };
 
-    var setCardHeight = function() {
+    $scope.setCartHeight = function() {
         var heightWindow = window.innerHeight - 90,
-            heightCard   = 103 * $scope.productsInCard.length + 70;
-        if (heightCard > heightWindow) {
+            heightCart   = 103 * $scope.productsInCart.length + 45;
+        if (heightCart > heightWindow) {
             $scope.cartHeight = {'height' : heightWindow + 'px'};
         } else {
             $scope.cartHeight = {};
         }
     };
 
-    var arrayObjectToString = function (array) {
+    $scope.arrayObjectToString = function (array) {
         var itemString = '';
 
         for (var i = 0; i < array.length; i++) {
@@ -188,7 +239,7 @@ app.controller('mainController', function($scope) {
         return itemString;
     };
 
-    var cartTotalSum = function(array) {
+    $scope.cartTotalSum = function(array) {
         var sum = 0;
 
         for (var i = 0; i < array.length; i++) {
@@ -197,4 +248,33 @@ app.controller('mainController', function($scope) {
 
         return sum;
     };
-});
+
+    $scope.setCartCookie = function(index, count) {
+        count = count || 1;
+        $cookies.put(index, count);
+    };
+
+    $scope.removeCartCookie = function(index) {
+        $cookies.remove(index);
+    };
+
+    $scope.setProductsInCart = function() {
+        var array = [];
+
+        for(var i=0; i < $scope.products.length; i++) {
+            if (typeof($cookies.get(i)) != 'undefined') {
+                var count = parseInt($cookies.get(i));
+
+                array.push($scope.addProductInCart(i, count));
+                $scope.products[i].classButton = $scope.classes.disabled;
+                $scope.products[i].cartTxt     = $scope.texts.textButtonOff;
+                $scope.products[i].left       -= count;
+                $scope.cartCount              += 1;
+            }
+        }
+
+        return array;
+    };
+    $scope.productsInCart = $scope.setProductsInCart();
+
+}]);
